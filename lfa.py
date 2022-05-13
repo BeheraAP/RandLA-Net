f7a116258639de4d27500e1a4c8bc4e3e4c64174
Annada Behera <annada.behera@niser.ac.in>
2022-05-13 19:03:51 +0530

    local spatial encoding added

--- lfa.py
+++ lfa.py
@@ -1,19 +1,48 @@
 import torch
 from torch import nn
+import numpy
+
+data = torch.tensor(numpy.loadtxt('./small.xyz', delimiter=' '),
+	dtype = torch.float32)
+locn = data[:,:3]
+feat = data[:,3:]
 
 class LocSE(nn.Module):
-	def __init__(self):
+	def __init__(self, k, point_cloud):
 		super().__init__()
-		pass
-
-	def forward(self, x):
-		pass
-
-	def debug():
-		pass
+		self.P = point_cloud
+		self.K = k
+		self.MLP = nn.Linear(10,6)
 
 
-class AttPool(nn.Module):
+	def rel_pos_enc(self, i, knn_loc):
+		pi  = self.P[i,:3]
+		r = []
+		for j in knn_loc:
+			diff = pi-j
+			dist = torch.norm(diff).unsqueeze(dim=0)
+			cat = torch.cat((pi,j,diff,dist))
+			r.append(self.MLP(cat))
+		return torch.vstack(r)
+
+	def knn(self, i):
+		diff = self.P[:,:3]- self.P[i,:3]
+		near_ks = diff.norm(dim=1).topk(self.K)
+		near = torch.index_select(self.P, 0, near_ks.indices)
+		return near
+
+	def forward(self, i):
+		N = self.knn(i)
+		R = self.rel_pos_enc(i, N[:,:3])
+		F = torch.cat((N,R), dim=1)
+		return F
+
+
+
+#debugging
+print(LocSE(10, data).forward(42).size())
+
+class AttentivePool(nn.Module):
 	def __init__(self):
 		super().__init__()
 		pass
@@ -25,6 +54,28 @@ class AttPool(nn.Module):
 		pass
 
 class DilRes(nn.Module):
+	def __init__(self):
+		super().__init__()
+		pass
+
+	def forward(self, x):
+		pass
+
+	def debug():
+		pass
+
+class LFA(nn.Module):
+	def __init__(self):
+		super().__init__()
+		pass
+
+	def forward(self, x):
+		pass
+
+	def debug():
+		pass
+
+class RandSample(nn.Module):
 	def __init__(self):
 		super().__init__()
 		pass
--- randla.py
+++ randla.py
@@ -1,29 +1,41 @@
 import torch
 from torch import nn
 from torch.utils.data import Dataset
-import pandas, numpy
 
-class rajarani(Dataset):
-	def __init__(self):
-		super().__init__()
-		data = pandas.read_table('./small.xyz', delimiter=' ')
+import numpy
+from lfa import LFA, RandSample
 
+class PointCloudRR():
+	def __init__(self, filename="./RRT2_group1_densified_point_cloud.xyz"):
+		# self.data = pandas.read_table(
+		# 	filename,
+		# 	delimiter=' ',
+		# 	names = ['X', 'Y', 'Z', 'R', 'G', 'B'],
+		# )
+		self.data = torch.tensor(numpy.loadtxt(filename, delimiter=' '))
 
-	def __len__(self):
-		pass
-
-	def __getitem__(self):
-		pass
-
+	def size(self):
+		return self.data.size()
 	def __repr__(self):
-		return "Rajarani dataset"
+		return '''Rajarani dataset:
+    type:    point cloud
+    format:  xyz
+    size:    %d
+    headers: X Y Z R G B'''%len(self.data)
+
 
 class RandLA(nn.Module):
-	def __init__(self):
+	def __init__(self, size=[1000,6]):
 		super().__init__()
+		N = size[0]
+		d = size[0]
+		self.layers = nn.ModuleList([
+			nn.Linear(N, N),
+		])
 		pass
 
 	def forward(self, x):
+		print(x.size())
 		pass
 
 	def debug(self):
