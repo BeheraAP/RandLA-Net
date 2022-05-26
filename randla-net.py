bd7126b585a60e25b1a8ab364fa2d7ad3e1b34a9
Annada Behera <annada.behera@niser.ac.in>
2022-05-26 16:57:26 +0530

    randla forward complete

--- randla-net.py
+++ randla-net.py
@@ -93,31 +93,31 @@ class LocSE(nn.Module):
 
 		# Gather neighbor
 		p,f = self.gather_neighbor(xyz, feat, idx)
-		print("After gathering neighbors: ", p.size(),f.size())
+		# print("After gathering neighbors: ", p.size(),f.size())
 
 		# Relative position encoding
 		# Input : xyz=[B, N, K, 3], idx=[B, N, K]
 		# Output: [B, N, K, d_in]
 		r = self.relative_pos_enc(p, idx)
-		print("Relative pos enc: ", r.size())
-		print("Features        : ", f.size())
+		# print("Relative pos enc: ", r.size())
+		# print("Features        : ", f.size())
 
 		# Concatenation
 		# Input : r=[B, N, K, d], f=[B, N, K, d]
 		# Output: rf=[B, N, K, 2d]
 		F = torch.cat((r,f), dim=3)
-		print("After concatenation: ", F.size());
+		# print("After concatenation: ", F.size());
 
 		# Returns a batch of B points with
 		# feat: [B, N, K, d_in]
 		return F
 
-B,N,k = 32, 100, 8
-LocSE(d_in=3, k=k)(
-	torch.randn(B, N, 3),
-	torch.randn(B, N, 3),
-	torch.tensor(torch.rand(B, N, k)*k, dtype=torch.int8)
-)
+# B,N,k = 32, 100, 8
+# LocSE(d_in=3, k=k)(
+# 	torch.randn(B, N, 3),
+# 	torch.randn(B, N, 3),
+# 	torch.tensor(torch.rand(B, N, k)*k, dtype=torch.int8)
+# )
 
 class AttentionPool(nn.Module):
 	def __init__(self, d_in, d_out):
@@ -131,7 +131,7 @@ class AttentionPool(nn.Module):
 		# Input
 		# F: [B, N, K, d_in]
 
-		print("Input to AttentionPool %s: %s" %(self.g, F.size()))
+		# print("Input to AttentionPool %s: %s" %(self.g, F.size()))
 		scores = self.g(F)
 		# Output
 		# F: [B, N, d_out]
@@ -153,42 +153,148 @@ class DilatedResBlock(nn.Module):
 			LocSE(d_out//2, k),
 			AttentionPool(d_out, d_out//2),
 			LocSE(d_out, k),
-			AttentionPool(d_out, d_out),
+			AttentionPool(2*d_out, d_out),
 			SharedMLP(d_out, 2*d_out),
 		])
 		self.shortcut = SharedMLP(d_in, 2*d_out)
 		self.lrelu = nn.LeakyReLU()
 
-	def forward(self, xyz, feat):
+	def forward(self, feat, xyz):
 		# Input
 		# xyz : [B, N, 3]
 		# feat: [B, N, d_in]
 
-		print(xyz.size(), feat.size())
 		idx = [i for i in range(10)]
 		sc = self.shortcut(feat)
+		# print("%-20s%s"%("Input", feat.size()), flush=True)
 		for L in self.layers:
 			if L.__class__.__name__ == 'LocSE':
 				feat = L(xyz, feat, idx)
 			else:
 				feat = L(feat)
 
-			print("%-20s%s"%(L.__class__.__name__, feat.size()),
-				flush=True)
+			# print("%-20s%s"%(L.__class__.__name__, feat.size()), flush=True)
 
-		# return self.lrelu(sc+feat)
+		return self.lrelu(sc+feat)
 
-DilatedResBlock(d_in=3, d_out=10, k=10)(
-	torch.rand(32,100,3),
-	torch.randn(32,100,3))
-exit()
-N,d_in = 100, 3
-DilatedResBlock(d_in, 6).forward(torch.randn(d_in, N, 3))
-class RandLANet(nn.Module):
-	def __init__(self,):
+# DilatedResBlock(d_in=3, d_out=10, k=10)(
+# 	torch.rand(32,100,3),
+# 	torch.randn(32,100,3)).size()
+# exit()
+class RandomSampling(nn.Module):
+	def __init__(self, factor):
 		super().__init__()
+		self.f = 1./factor
 
-	def forward(self,):
-		pass
+	def forward(self, x):
+		N = x.size()[-2]
+		idx = torch.randperm(N)[:int(N*self.f)]
+		return x.index_select(1, idx)
+class UpSampling(nn.Module):
+	def __init__(self, factor):
+		super().__init__();
+		self.f = factor
+
+	def forward(self, x):
+		return x
+# Sampling(factor=.3)(torch.randn(32, 1000, 128), "dummy")
+# exit()
+
+class RandLANet(nn.Module):
+	def __init__(self, d_in, k, n_class):
+		super().__init__()
+		self.d_in = d_in
+		self.k = k
+		factor = 4
+
+		# Begin
+		self.ll0 = nn.Linear(d_in, 8)
+
+		# Encoding modules
+		self.db0 = DilatedResBlock(8, 16, k)
+		self.rs0 = RandomSampling(factor)
+		self.db1 = DilatedResBlock(32, 64, k)
+		self.rs1 = RandomSampling(factor)
+		self.db2 = DilatedResBlock(128, 128, k)
+		self.rs2 = RandomSampling(factor)
+		self.db3 = DilatedResBlock(256, 256, k)
+		self.rs3 = RandomSampling(factor)
+
+		# Center module
+		self.mlp = SharedMLP(512, 512)
+
+		# Decoding modules
+		decoder = {'transpose': False}
+		self.sm0 = SharedMLP(512, 256, *decoder)
+		self.us0 = UpSampling(factor)
+		self.sm1 = SharedMLP(512, 256, *decoder)
+		self.us1 = UpSampling(factor)
+		self.sm2 = SharedMLP(512, 256, *decoder)
+		self.us2 = UpSampling(factor)
+		self.sm3 = SharedMLP(512, 256, *decoder)
+		self.us3 = UpSampling(factor)
+
+		# End
+		self.ll1 = nn.Linear(8, 64)
+		self.ll2 = nn.Linear(64, 32)
+		self.dp0 = nn.Dropout(p=.5)
+		self.ll3 = nn.Linear(32, n_class);
 
 
+
+
+	def forward(self, x):
+		# Input: [B, N, d_in]
+
+		xyz = x[:3]
+
+		x = self.ll0(x)
+		sk0 = x.clone()
+
+		x = self.db0(x, xyz)
+		x = self.rs0(x)
+		sk1 = x.clone()
+
+		x = self.db1(x, xyz)
+		x = self.rs1(x)
+		sk2 = x.clone()
+
+		x = self.db2(x, xyz)
+		x = self.rs2(x)
+		sk3 = x.clone()
+
+		x = self.db3(x, xyz)
+		x = self.rs3(x)
+
+		x = self.mlp(x)
+
+		x = self.us0(x)
+		print(x.size(), sk3.size(), flush=True)
+		exit()
+		x = self.sm0(x+sk3)
+
+		x = self.us1(x)
+		x = self.sm1(x+sk2)
+
+		x = self.us2(x)
+		x = self.sm2(x+sk1)
+
+		x = self.us3(x)
+		x = self.sm3(x+sk0)
+
+		x = self.ll1(x)
+
+		x = self.ll2(x)
+		x = self.dp0(x)
+
+		x = self.ll3(x)
+
+		return x;
+
+
+
+
+
+		return x
+
+RandLANet(d_in=6, k=16, n_class=10)(torch.randn(32, 1000, 6))
